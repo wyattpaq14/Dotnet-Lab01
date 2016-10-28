16dfe09a6100c32a25bec9e0181564ed6dc9c62f
paqui <paqui@DESKTOP-0F5HTGM>
2016-10-28 10:19:05 -0400

    Validation

--- DotNet Lab1/Admin_Pages/User.aspx
+++ DotNet Lab1/Admin_Pages/User.aspx
@@ -2,6 +2,150 @@
 
 
 
-<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <h3>I am User</h3>
+<asp:Content ID="userForm" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <%--firstName validator, textbox and label--%>
+    <div class="form-group col-lg-8 col-md-offset-2">
+        <asp:Label ID="lblFirstName" runat="server" Text="First Name" CssClass="col-lg-2 control-label"></asp:Label>
+        <div class="col-lg-10">
+            <asp:TextBox ID="txtFirstName" runat="server" CssClass="form-control"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="rfvFirstName" runat="server" Display="None" ControlToValidate="txtFirstName" ErrorMessage="First Name is required"></asp:RequiredFieldValidator>
+        </div>
+    </div>
+    <br />
+    <br />
+
+    <%--lastName validator, textbox and label--%>
+    <div class="form-group col-lg-8 col-md-offset-2">
+        <asp:Label ID="lblLastName" runat="server" Text="Last Name" CssClass="col-lg-2 control-label"></asp:Label>
+        <div class="col-lg-10">
+            <asp:TextBox ID="txtLastName" runat="server" CssClass="form-control"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="rfvLastName" runat="server" Display="None" ControlToValidate="txtLastName" ErrorMessage="Last Name is required"></asp:RequiredFieldValidator>
+        </div>
+    </div>
+    <br />
+    <br />
+
+    <%--Address 1 validator, textbox and label--%>
+    <div class="form-group col-lg-8 col-md-offset-2">
+        <asp:Label ID="lblAddress1" runat="server" Text="Address 1" CssClass="col-lg-2 control-label"></asp:Label>
+        <div class="col-lg-10">
+            <asp:TextBox ID="txtAddress1" runat="server" CssClass="form-control"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="rfvAddress1" runat="server" Display="None" ControlToValidate="txtAddress1" ErrorMessage="Address 1 is required"></asp:RequiredFieldValidator>
+        </div>
+    </div>
+    <br />
+    <br />
+
+    <%--Address 2 validator, textbox and label--%>
+    <div class="form-group col-lg-8 col-md-offset-2">
+        <asp:Label ID="lblAddress2" runat="server" Text="Address 2" CssClass="col-lg-2 control-label"></asp:Label>
+        <div class="col-lg-10">
+            <asp:TextBox ID="txtAddress2" runat="server" CssClass="form-control"></asp:TextBox>
+            <%--not required--%>
+            <%--<asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" Display="None" ControlToValidate="txtAddress2" ErrorMessage="Address 2 is required"></asp:RequiredFieldValidator>--%>
+        </div>
+    </div>
+    <br />
+    <br />
+
+    <%--city validator, textbox and label--%>
+    <div class="form-group col-lg-8 col-md-offset-2">
+        <asp:Label ID="lblCity" runat="server" Text="City" CssClass="col-lg-2 control-label"></asp:Label>
+        <div class="col-lg-10">
+            <asp:TextBox ID="txtCity" runat="server" CssClass="form-control"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="rfvCity" runat="server" Display="None" ControlToValidate="txtCity" ErrorMessage="City is required"></asp:RequiredFieldValidator>
+        </div>
+    </div>
+    <br />
+    <br />
+
+    <%--state validator, textbox and label--%>
+    <div class="form-group col-lg-8 col-md-offset-2">
+        <asp:Label ID="lblState" runat="server" Text="State" CssClass="col-lg-2 control-label"></asp:Label>
+        <div class="col-lg-10">
+            <asp:TextBox ID="txtState" runat="server" CssClass="form-control"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="rfvState" runat="server" Display="None" ControlToValidate="txtState" ErrorMessage="State is required"></asp:RequiredFieldValidator>
+        </div>
+    </div>
+    <br />
+    <br />
+
+    <%--Zip validator, textbox and label--%>
+    <div class="form-group col-lg-8 col-md-offset-2">
+        <asp:Label ID="lblZip" runat="server" Text="Zip Code" CssClass="col-lg-2 control-label"></asp:Label>
+        <div class="col-lg-10">
+            <asp:TextBox ID="txtZip" runat="server" CssClass="form-control"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="rfvZip" runat="server" Display="None" ControlToValidate="txtZip" ErrorMessage="Zip Code is required"></asp:RequiredFieldValidator>
+        </div>
+    </div>
+    <br />
+    <br />
+
+    <%--password validator, textbox and label--%>
+    <div class="form-group col-lg-8 col-md-offset-2">
+        <asp:Label ID="lblPassword" runat="server" Text="Password" CssClass="col-lg-2 control-label"></asp:Label>
+        <div class="col-lg-10">
+            <asp:TextBox ID="txtPassword" runat="server" CssClass="form-control" TextMode="Password"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="rfvPassword" runat="server" Display="None" ControlToValidate="txtPassword" ErrorMessage="Password is required"></asp:RequiredFieldValidator>
+        </div>
+    </div>
+    <br />
+    <br />
+
+    <%--password confirm validator, textbox and label--%>
+    <div class="form-group col-lg-8 col-md-offset-2">
+        <asp:Label ID="lblPasswordConfirm" runat="server" Text="Confirm Password" CssClass="col-lg-2 control-label"></asp:Label>
+        <div class="col-lg-10">
+            <asp:TextBox ID="txtPasswordConfirm" runat="server" CssClass="form-control" TextMode="Password"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="rfvPasswordConfirm" runat="server" Display="None" ControlToValidate="txtPasswordConfirm" ErrorMessage="Password confirm is required"></asp:RequiredFieldValidator>
+        </div>
+    </div>
+    <br />
+    <br />
+
+    <%--email validator, textbox and label--%>
+    <div class="form-group col-lg-8 col-md-offset-2">
+        <asp:Label ID="lblEmail" runat="server" Text="Email" CssClass="col-lg-2 control-label"></asp:Label>
+        <div class="col-lg-10">
+            <asp:TextBox ID="txtEmail" runat="server" CssClass="form-control"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="rfvEmail" runat="server" Display="None" ControlToValidate="txtEmail" ErrorMessage="Email is required"></asp:RequiredFieldValidator>
+        </div>
+    </div>
+    <br />
+    <br />
+    <%--email confirm validator, textbox and label--%>
+    <div class="form-group col-lg-8 col-md-offset-2">
+        <asp:Label ID="lblEmailConfirm" runat="server" Text="Confirm Email" CssClass="col-lg-2 control-label"></asp:Label>
+        <div class="col-lg-10">
+            <asp:TextBox ID="txtEmailConfirm" runat="server" CssClass="form-control"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="rfvEmailConfirm" runat="server" Display="None" ControlToValidate="txtEmailConfirm" ErrorMessage="Email confirm is required"></asp:RequiredFieldValidator>
+        </div>
+    </div>
+    <br />
+    <br />
+
+    <%--phone validator, textbox and label--%>
+    <div class="form-group col-lg-8 col-md-offset-2">
+        <asp:Label ID="lblPhone" runat="server" Text="Phone Number" CssClass="col-lg-2 control-label"></asp:Label>
+        <div class="col-lg-10">
+            <asp:TextBox ID="txtPhone" runat="server" CssClass="form-control"></asp:TextBox>
+            <label>
+                <input type="checkbox" />
+                Checkbox
+            </label>
+            <asp:RequiredFieldValidator ID="rfvPhone" runat="server" Display="None" ControlToValidate="txtPhone" ErrorMessage="Phone Number is required"></asp:RequiredFieldValidator>
+        </div>
+    </div>
+    <br />
+    <br />
+
+
+    <asp:Button ID="btnLogin" runat="server" CssClass="col-lg-4 btn btn-default col-md-offset-2" Text="Login" />
+    <asp:Button ID="btnForgotPassword" runat="server" CssClass="col-lg-4 btn btn-default" Text="Forgot Password" />
+    <asp:ValidationSummary ID="vsForm" ShowSummary="true" ShowMessageBox="true" runat="server" />
+
+    <br />
+    <br />
+
 </asp:Content>
--- DotNet Lab1/Admin_Pages/User.aspx.designer.cs
+++ DotNet Lab1/Admin_Pages/User.aspx.designer.cs
@@ -11,5 +11,347 @@ namespace DotNet_Lab1.Pages {
     
     
     public partial class User {
+        
+        /// <summary>
+        /// lblFirstName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblFirstName;
+        
+        /// <summary>
+        /// txtFirstName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFirstName;
+        
+        /// <summary>
+        /// rfvFirstName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvFirstName;
+        
+        /// <summary>
+        /// lblLastName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblLastName;
+        
+        /// <summary>
+        /// txtLastName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtLastName;
+        
+        /// <summary>
+        /// rfvLastName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvLastName;
+        
+        /// <summary>
+        /// lblAddress1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblAddress1;
+        
+        /// <summary>
+        /// txtAddress1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtAddress1;
+        
+        /// <summary>
+        /// rfvAddress1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvAddress1;
+        
+        /// <summary>
+        /// lblAddress2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblAddress2;
+        
+        /// <summary>
+        /// txtAddress2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtAddress2;
+        
+        /// <summary>
+        /// lblCity control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblCity;
+        
+        /// <summary>
+        /// txtCity control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCity;
+        
+        /// <summary>
+        /// rfvCity control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvCity;
+        
+        /// <summary>
+        /// lblState control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblState;
+        
+        /// <summary>
+        /// txtState control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtState;
+        
+        /// <summary>
+        /// rfvState control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvState;
+        
+        /// <summary>
+        /// lblZip control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblZip;
+        
+        /// <summary>
+        /// txtZip control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtZip;
+        
+        /// <summary>
+        /// rfvZip control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvZip;
+        
+        /// <summary>
+        /// lblPassword control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblPassword;
+        
+        /// <summary>
+        /// txtPassword control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPassword;
+        
+        /// <summary>
+        /// rfvPassword control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvPassword;
+        
+        /// <summary>
+        /// lblPasswordConfirm control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblPasswordConfirm;
+        
+        /// <summary>
+        /// txtPasswordConfirm control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPasswordConfirm;
+        
+        /// <summary>
+        /// rfvPasswordConfirm control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvPasswordConfirm;
+        
+        /// <summary>
+        /// lblEmail control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblEmail;
+        
+        /// <summary>
+        /// txtEmail control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
+        
+        /// <summary>
+        /// rfvEmail control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvEmail;
+        
+        /// <summary>
+        /// lblEmailConfirm control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblEmailConfirm;
+        
+        /// <summary>
+        /// txtEmailConfirm control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmailConfirm;
+        
+        /// <summary>
+        /// rfvEmailConfirm control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvEmailConfirm;
+        
+        /// <summary>
+        /// lblPhone control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblPhone;
+        
+        /// <summary>
+        /// txtPhone control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPhone;
+        
+        /// <summary>
+        /// rfvPhone control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvPhone;
+        
+        /// <summary>
+        /// btnLogin control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnLogin;
+        
+        /// <summary>
+        /// btnForgotPassword control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnForgotPassword;
+        
+        /// <summary>
+        /// vsForm control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ValidationSummary vsForm;
     }
 }
