b363c95261b0a62504a45f607cc11074a9c6a695
paqui <paqui@DESKTOP-0F5HTGM>
2016-12-10 20:38:03 -0500

    Finished UpdateGuest

--- DotNet Lab1/App_Code/Guest.cs
+++ DotNet Lab1/App_Code/Guest.cs
@@ -133,8 +133,41 @@ namespace DotNet_Lab1.App_Code
             return isInserted;
         }
 
-        public static bool UpdateGuest()
+        public static bool UpdateGuest(DataTable guest)
         {
+            int id = 0;
+            SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["SQL Server"].ConnectionString);
+            SqlCommand cmd = new SqlCommand("guests_Update", cn);
+
+            cmd.Parameters.Add("@guest_email", SqlDbType.VarChar).Value = guest.Rows[0]["guest_email"];
+            cmd.Parameters.Add("@guest_first", SqlDbType.VarChar).Value = guest.Rows[0]["guest_first"];
+            cmd.Parameters.Add("@guest_last", SqlDbType.VarChar).Value = guest.Rows[0]["guest_last"];
+            cmd.Parameters.Add("@guest_salt", SqlDbType.VarChar).Value = guest.Rows[0]["guest_salt"];
+            cmd.Parameters.Add("@guest_pwd", SqlDbType.VarChar).Value = guest.Rows[0]["guest_pwd"];
+            cmd.Parameters.Add("@guest_phone", SqlDbType.VarChar).Value = guest.Rows[0]["guest_phone"];
+
+            // Open the database connection and execute the command
+            try
+            {
+                //opens connection to database, most failures happen here
+                //check connection string for proper settings
+                cn.Open();
+                //data adapter object is trasport link between data source and 
+                //execute the non-query stored procedure
+                cmd.ExecuteNonQuery();
+                id = Convert.ToInt32(cmd.Parameters["@guest_id"].Value);
+            }
+            catch (Exception exc)
+            {
+                //just put here to make debugging easier, can look at error directly
+                exc.ToString();
+            }
+            finally
+            {
+                //must always close connections
+                cn.Close();
+            }
+
             bool isUpdated = false;
             return isUpdated;
         }
